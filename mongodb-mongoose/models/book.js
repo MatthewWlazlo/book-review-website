f71abd1d976d870c8f78dc5a5e51e03ef24aa3b1
Matt Wlazlo <mwlazlo414@gmail.com>
2025-12-07 17:22:43 -0500

    Fixed schema

--- mongodb-mongoose/models/book.js
+++ mongodb-mongoose/models/book.js
@@ -9,14 +9,18 @@ schema that represents a a book
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 const bookSchema = new Schema({
-  name: String,
-  email: String,
-  title: String,
-  published: String,
-  author: String,
-  summary: String,
-  reviews: [String],
-}, {
+  book: {
+    title: String,
+    published: String,
+    author: String,
+    summary: String,
+    reviews: [{
+      name: String,
+      email: String,
+      review: String
+    }],
+  },
+  }, {
   timestamps: true
 });
 const Book = model('Book', bookSchema);
